5ac0c2a965f6ffd8a7ae6f12550055e724771f35
danielprince.d <d.danielprince2003@gmail.com>
2025-12-25 09:38:38 +0530

    add customer model

---
+++ lib/models/Customer.dart
@@ -0,0 +1,32 @@
+
+
+import 'package:service_desk/controller/db_initializer.dart';
+
+class Customer{
+  final int? customerId;
+  final String customerName;
+  final String mobilNumber;
+
+  Customer({this.customerId, required this.customerName, required this.mobilNumber});
+
+  Map<String, dynamic> toMap(){
+    var map = <String, dynamic>{
+      'CustomerId': customerId,
+      'CustomerName': customerName,
+      'MobilNumber': mobilNumber,
+    };
+    return map;
+  }
+
+  bool addorUpdateCustomer() {
+    DBInitializer.instance.db.then((database) async {
+      customerId == null ? 
+        await database.insert('CustomerTable', toMap()) : 
+        await database.update('CustomerTable', toMap(), 
+          where: 'CustomerId = ?', whereArgs: [customerId]);
+      return true;
+    });
+    return false;
+  }
+
+}
