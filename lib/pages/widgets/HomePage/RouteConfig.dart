a7d16c759fe0ce06978fda4a0fd8fdfab9e4e9a0
danielprince.d <d.danielprince2003@gmail.com>
2025-12-25 11:43:23 +0530

    configure routes in RouteConfig file

--- lib/pages/widgets/HomePage/RouteConfig.dart
+++ lib/pages/widgets/HomePage/RouteConfig.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+
 import 'package:service_desk/pages/widgets/Services/ServiceListView.dart';
+import 'package:service_desk/pages/widgets/Services/ServiceDetailsPage.dart';
+import 'package:service_desk/pages/widgets/Services/ServiceEditPage.dart';
+
+import 'package:service_desk/pages/widgets/Customers/CustomerListView.dart';
+import 'package:service_desk/pages/widgets/Customers/CustomerEditPage.dart';
+import 'package:service_desk/pages/widgets/Customers/CustomerDetailsPage.dart';
+
+import 'package:service_desk/pages/widgets/Transactions/TransactionListView.dart';
+import 'package:service_desk/pages/widgets/Transactions/TransactionEditPage.dart';
+import 'package:service_desk/pages/widgets/Transactions/TransactionDetailsPage.dart'; 
 
 class Routeconfig{
     static Map<String , Widget Function(BuildContext)> all(BuildContext context){
       return {
+        // Service 
         ServiceListPage.routeName: (context) => const ServiceListPage(),
+        ServiceDetailsPage.routeName: (context) => const ServiceDetailsPage(),
+        ServiceEditPage.routeName: (context) => const ServiceEditPage(),
+
+        // Customer 
+        CustomerListView.routeName: (context) => const CustomerListView(),
+        CustomerDetailsPage.routeName: (context) => const CustomerDetailsPage(),
+        CustomerEditPage.routeName: (context) => const CustomerEditPage(),
+
+        // Transaction
+        TransactionListView.routeName: (context) => const TransactionListView(),
+        TransactionDetailsPage.routeName: (context) => const TransactionDetailsPage(),
+        TransactionEditPage.routeName: (context) => const TransactionEditPage(),
         
       };
     }
